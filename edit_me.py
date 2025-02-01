829da3b3ddad2dcfc1961ad84e3846fbe91c87a5
Ivan <adam1shark@gmail.com>
2025-01-31 22:54:16 -0600

    Finished Lab2

--- edit_me.py
+++ edit_me.py
@@ -1,11 +1,24 @@
+# Lists for testing
+mylist = [2, 2, 4, 200]  # Increasing 
+mylist1 = [20, 15, 5, -5]  # Decreasing 
+mylist2 = [3, 5, 4, 6]  # Not Monotonic
+
+def isincreasing(lst):
+    for i in range(len(lst) - 1):
+        if lst[i] > lst[i + 1]:
+            return False
+    return True
+
+def isdecreasing(lst):
+    for i in range(len(lst) - 1):
+        if lst[i] < lst[i + 1]:
+            return False
+    return True
 
 def monotonic(lst):
-    """Return True if lst is monotonic; return False, otherwise."""
-    #
-    # YOUR IMPLEMENTATION GOES HERE
-    #
+    return isincreasing(lst) or isdecreasing(lst)
 
-#
-# Feel free to replace these comments with
-# code that tests your function monotonic.
-#
+print(monotonic(mylist))   # True
+print(monotonic(mylist1))  # True
+print(monotonic(mylist2))  # False
+#Ivan Adamchuk
